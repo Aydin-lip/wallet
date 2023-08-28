8a4d0557dcd97ccda05f26e488d8379a96c88b5c
Aydin <ms1166760@gmail.com>
2023-08-28 11:59:37 +0330

    Add bg second section for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import FirstSection from "@/components/home/firstSection";
+import SecondSection from "@/components/home/secondSection";
 
 const Home = () => {
   return (
     <>
       <div className="h-[400vh] bg-background">
         <FirstSection />
+        <SecondSection />
       </div>
     </>
   )
