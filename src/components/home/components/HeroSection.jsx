1c37b37c5127a709c9077e9f9d024db25e276cc2
Aydin <ms1166760@gmail.com>
2023-09-02 14:52:47 +0330

    Set responsive first section

--- src/components/home/components/HeroSection.jsx
+++ src/components/home/components/HeroSection.jsx
@@ -42,22 +42,22 @@ export default function HeroSection() {
 
   return (
     <div className="text-white body-font max-w-[1200px] mx-auto h-full mt-12">
-      <div className="container mx-auto w-full h-full flex flex-col lg:flex-row gap-10 justify-center items-center">
+      <div className="container mx-auto w-full h-full flex flex-col lg:flex-row sm:gap-6 md:gap-10 justify-center items-center">
         <div className=" md:pr-16 lg:pr-0 pr-0">
-          <h1 className="title-font text-3xl lg:text-5xl mt-4 font-bold">
+          <h1 className="title-font min-[400px]:text-2xl lg:text-5xl mt-10 min-[400px]:mt-12 lg:mt-4 font-bold">
             نقل و انتقال مالی جهانی
           </h1>
-          <p className="leading-relaxed mt-12 text-xl">
+          <p className="leading-relaxed mt-4 min-[400px]:mt-12 text-sm min-[400px]:text-xl max-sm:w-full max-lg:w-2/3">
             در هزینه‌‌ها صرفه‌‌جویی کنید. بهترین نرخ‌‌های ارز را به‌‌دست آورید.
             ارزان‌‌ترین ارائه‌‌دهنده انتقال را پیدا کنید
           </p>
         </div>
-        <div className="lg:w-1/2 relative rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 bg-[#00000040]">
-          <div className="relative mb-4">
+        <div className="lg:w-1/2 relative rounded-lg p-4 sm:p-8 flex flex-col md:ml-auto w-full mt-8 min-[400px]:mt-10 md:mt-0 bg-[#00000040]">
+          <div className="relative mb-2 max-[400px]:text-sm min-[400px]:mb-4">
             <label htmlFor="full-name" className="leading-7 text-sm">
               بیایید پول شما را انتقال دهیم
             </label>
-            <div className="flex flex-row-reverse justify-between items-center border border-[#ccc] rounded-md bg-[#ffffff33] p-2 selectBoxContainer">
+            <div className="mt-2 min-[400px]:mt-4 flex flex-row-reverse justify-between items-center border border-[#ccc] rounded-md bg-[#ffffff33] min-[400px]:p-2 selectBoxContainer">
               <CustomSelectInput
                 options={currencies}
                 selectedOption={selectedCurrency}
@@ -72,11 +72,11 @@ export default function HeroSection() {
               />
             </div>
           </div>
-          <div className="relative mb-4">
+          <div className="relative mb-2 max-[400px]:text-sm min-[400px]:mb-4">
             <label htmlFor="email" className="leading-7 text-sm">
               از کشور
             </label>
-            <div className="flex flex-row-reverse justify-between items-center border border-[#ccc] rounded-md bg-[#ffffff33] p-2">
+            <div className="mt-2 min-[400px]:mt-4 flex flex-row-reverse justify-between items-center border border-[#ccc] rounded-md bg-[#ffffff33] min-[400px]:p-2">
               <CustomSelectInput
                 options={countries}
                 selectedOption={selectedOrigin}
@@ -85,11 +85,11 @@ export default function HeroSection() {
               {selectedOrigin && <CountryFlag country={selectedOrigin.value} />}
             </div>
           </div>
-          <div className="relative mb-4">
+          <div className="relative mb-2 max-[400px]:text-sm min-[400px]:mb-4">
             <label htmlFor="email" className="leading-7 text-sm">
               به کشور
             </label>
-            <div className="flex flex-row-reverse justify-between items-center border border-[#ccc] rounded-md bg-[#ffffff33] p-2">
+            <div className="mt-2 min-[400px]:mt-4 flex flex-row-reverse justify-between items-center border border-[#ccc] rounded-md bg-[#ffffff33] min-[400px]:p-2">
               <CustomSelectInput
                 options={countries}
                 selectedOption={selectedDestination}
@@ -101,7 +101,7 @@ export default function HeroSection() {
             </div>
           </div>
           <button
-            className="text-white bg-active-them border-0 py-2 px-8 focus:outline-none hover:bg-active-them-dark rounded text-lg cursor-pointer"
+            className="max-[400px]:text-sm text-white bg-active-them border-0 py-2 px-8 focus:outline-none hover:bg-active-them-dark rounded text-lg cursor-pointer"
           >
             مقایسه سرویس‌‌دهنده‌‌‌ها
           </button>
