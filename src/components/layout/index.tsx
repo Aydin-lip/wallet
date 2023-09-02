2e65fb5234fb5b72def43c71ec3de53d9a72bed6
Aydin <ms1166760@gmail.com>
2023-09-02 09:33:23 +0330

    Add lift & button change theme

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,14 @@
 import Footer from "../footer"
+import LiftComponent from "../lift"
 import Navbar from "../navbar"
+import ThemeComponent from "../theme"
 
 const Layout = ({ children }: { children: JSX.Element }) => {
   return (
     <>
       <Navbar />
+      <ThemeComponent />
+      <LiftComponent />
       {children}
       <Footer />
     </>
---
+++ src/components/lift/index.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useMemo, useState } from "react";
+import { MdOutlineKeyboardArrowUp } from "react-icons/md";
+
+const Style = `
+.animation-lift {
+  animation: anim-lift 1s ease-in-out infinite alternate;
+}
+@keyframes anim-lift {
+  0% {
+    transform: translateY(0px);
+  }
+  100% {
+    transform: translateY(-10px);
+  }
+}
+`
+
+const LiftComponent = () => {
+  const [show, setShow] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', () => {
+        if (window?.screen.height < window?.scrollY) {
+          setShow(true)
+        } else {
+          setShow(false)
+        }
+      });
+    }
+  }, [])
+
+  return (
+    <>
+      <style children={Style}></style>
+      <div
+        className={`${show ? '' : 'opacity-0 bottom-40'} transition-all duration-500 animation-lift fixed left-10 bottom-10 z-[2] bg-active-them w-12 h-12 rounded-[62%_38%_46%_54%_/_60%_63%_37%_40%] shadow-[0px_3px_10px_#868686] cursor-pointer`}
+        onClick={() => window.scrollTo(0, 0)}
+      >
+        <span className="h-full w-full flex justify-center items-center">
+          <MdOutlineKeyboardArrowUp className="w-6 h-6 text-white" />
+        </span>
+      </div>
+    </>
+  )
+}
+
+export default LiftComponent
